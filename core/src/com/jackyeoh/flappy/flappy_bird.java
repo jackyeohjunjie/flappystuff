12a3964e619b5b821a2865c5329cb3884ee98484
Jack Yeoh <jack.yeoh@hotmail.com>
2021-01-18 01:32:23 +0800

    Added Gameover Image

--- core/src/com/jackyeoh/flappy/flappy_bird.java
+++ core/src/com/jackyeoh/flappy/flappy_bird.java
@@ -9,11 +9,9 @@ import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 import com.badlogic.gdx.graphics.g2d.TextureAtlas;
 import com.badlogic.gdx.graphics.g2d.Animation;
 import com.badlogic.gdx.graphics.g2d.TextureRegion;
-import com.badlogic.gdx.graphics.glutils.ShapeRenderer;
 import com.badlogic.gdx.math.Circle;
 import com.badlogic.gdx.math.Intersector;
 import com.badlogic.gdx.math.Rectangle;
-import com.badlogic.gdx.scenes.scene2d.actions.DelayAction;
 
 import java.util.Random;
 
